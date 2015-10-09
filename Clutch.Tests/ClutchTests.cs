fb5d8741dadbd2987d68195b3ae86f8d180f3f6a
Tom Elmore <tom.elmore@easyroommate.com>
2015-10-09 16:01:23 +0100

    added initial test

---
+++ Clutch.Tests/ClutchTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Clutch.Tests
+{
+    [TestFixture]
+    public class ClutchTests
+    {
+        [Test]
+        public void RetrievesModelFromRoot()
+        {
+            Room room = new FluentClient("http://local.property.erm-api.com/v1/").Get<Room>().Result;
+
+            Assert.IsNotNull(room);
+
+            Assert.AreEqual("h234234872359283", room.Id);
+
+            Assert.AreEqual("London", room.City);
+        }
+    }
+
+    public class Room
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+
+        public string User { get; set; }
+
+        public int TypeId { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public DateTime DateAvailable { get; set; }
+
+        public string DwellingCode { get; set; }
+        public List<Amenity> Amenities { get; set; }
+
+        public string MetroCode { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public float MapX { get; set; }
+        public float MapY { get; set; }
+
+        public string Description { get; set; }
+        public string Comment { get; set; }
+    }
+
+    public class Amenity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Clutch/FluentClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Clutch
+{
+    public class FluentClient
+    {
+        private readonly string _rootUrl;
+
+        public FluentClient(string rootUrl)
+        {
+            _rootUrl = rootUrl;
+        }
+
+        public async Task<T> Get<T>()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_rootUrl);
+
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("rooms/1");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<T>();
+                }
+               
+                return default(T) ;
+            }
+        }
+    }
+}
