37f197dd3808fb54330b97673a57318ff5b27627
Tom Elmore <tom.elmore@easyroommate.com>
2015-10-20 17:45:53 +0100

    upgraded to 4.5 - added extension methods to make up for those deprecated in the System.Net.Http.Formatting dll

---
+++ Clutch/HttpClientExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Clutch
+{
+    static class HttpClientExtensions
+    {
+        public static Task<HttpResponseMessage> PostAsJsonAsync(this HttpClient b, string url, object model)
+        {
+            string json = JsonConvert.SerializeObject(model);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return b.PostAsync(url, content);
+        }
+    }
+}
---
+++ Clutch/HttpContentExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Clutch
+{
+    static class HttpContentExtensions
+    {
+        public static async Task<T> ReadAsAsync<T>(this HttpContent content)
+        {
+            string json = await content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
